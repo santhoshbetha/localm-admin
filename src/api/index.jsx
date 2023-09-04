c84fefb6d5136faae6bc5cf3b090f8cc8e63103d
santhoshbetha <santhosh.betha@gmail.com>
2023-09-04 00:32:51 -0500

    backennd chnage to cyclic

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,3 +1,5 @@
 //export const url = "https://nodejsclusters-142702-0.cloudclusters.net/api/v1"
 
-export const url = "http://localhost:7700/api/v1"
+//export const url = "http://localhost:7700/api/v1"
+
+export const url = "https://brick-red-penguin-boot.cyclic.app/api/v1"
