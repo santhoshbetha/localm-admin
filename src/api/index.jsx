c2f61480bf610e78954e35a33b294c195acc7b3a
Santhosh Betha <santhosh.betha@gmail.com>
2024-03-10 14:28:11 -0500

    cities updates etc

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,5 +1,1 @@
-//export const url = "https://nodejsclusters-142702-0.cloudclusters.net/api/v1"
-
-//export const url = "http://localhost:7700/api/v1"
-
-export const url = "https://nodejsclusters-142702-0.cloudclusters.net/api/v1"
+export const url = "http://localhost:7700/api/v1"
